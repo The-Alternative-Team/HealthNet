d98f66c74773263fbb974ed835ec1e82eb83b78b
Devin Matte <devinmatte@gmail.com>
2017-03-01 21:19:12 +0000

    Updated UI

--- HealthApp/urls.py
+++ HealthApp/urls.py
@@ -4,5 +4,11 @@ from . import views
 
 # app_name = 'HealthApp'
 urlpatterns = [
-    url(r'^$', views.index),
+    url(r'^doctor', views.doctor, name="doctor"),
+    url(r'^patient', views.patient, name="patient"),
+    url(r'^login', views.login, name="login"),
+    url(r'^blank', views.blank, name="blank"),
+    url(r'^forms', views.forms, name="forms"),
+    url(r'^grid', views.grid, name="grid"),
+    url(r'^tables', views.tables, name="tables"),
 ]
--- HealthApp/views.py
+++ HealthApp/views.py
@@ -1,5 +1,29 @@
 from django.shortcuts import render
 
 
-def index(request):
-    return render(request, 'HealthApp/index.html')
+def doctor(request):
+    return render(request, 'HealthApp/doctorIndex.html')
+
+
+def patient(request):
+    return render(request, 'HealthApp/patientIndex.html')
+
+
+def login(request):
+    return render(request, 'HealthApp/login.html')
+
+
+def blank(request):
+    return render(request, 'HealthApp/blank.html')
+
+
+def forms(request):
+    return render(request, 'HealthApp/forms.html')
+
+
+def grid(request):
+    return render(request, 'HealthApp/grid.html')
+
+
+def tables(request):
+    return render(request, 'HealthApp/tables.html')

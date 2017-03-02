40943cb73acb3562ad88bfc1f7ef5a864a1a8887
Shadman Nazmuddin <shadman97@gmail.com>
2017-03-02 02:20:46 +0000

    Fixed hospital attribute error in Patient

--- HealthApp/models/Patient.py
+++ HealthApp/models/Patient.py
@@ -5,13 +5,14 @@ from .Doctor import Doctor
 
 
 class Patient(User):
-    hospital = models.ForeignKey(Hospital, default=None, verbose_name='Hospital')
+    hospital = models.ForeignKey(Hospital, related_name="current_hospital", default=None, verbose_name='Hospital')
     doctor = models.ForeignKey(Doctor, default=None, verbose_name='Doctor')
     prescription = []
     test_results = []
     med_info = []
     appointments = []
-    desired_hospital = models.ForeignKey(Hospital, default=None, verbose_name='Desired Hospital')
+    desired_hospital = models.ForeignKey(Hospital, related_name="desired_hospital", default=None,
+                                         verbose_name='Desired Hospital')
     e_cont_fname = models.CharField(max_length=50, verbose_name="Emergency Contact: First Name")
     e_cont_lname = models.CharField(max_length=50, verbose_name="Emergency Contact: Last Name")
     e_cont_home_phone = models.BigIntegerField(help_text="No spaces or dashes",

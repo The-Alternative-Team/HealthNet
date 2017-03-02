95b5fec39f191cba3d30bb34b8a8043b1e5e7f3d
Shadman Nazmuddin <shadman97@gmail.com>
2017-03-02 02:06:30 +0000

    Added Appointment, Doctor, and Nurse class. Edited inheritences to reflect UML Diagram.

--- HealthApp/models/Patient.py
+++ HealthApp/models/Patient.py
@@ -1,14 +1,17 @@
 from django.db import models
+from .User import User
+from .Hospital import Hospital
+from .Doctor import Doctor
 
 
-class Patient(models.Model):
-    hospital = models.CharField(max_length=50)
-    doctor = models.CharField(max_length=50)
+class Patient(User):
+    hospital = models.ForeignKey(Hospital, default=None, verbose_name='Hospital')
+    doctor = models.ForeignKey(Doctor, default=None, verbose_name='Doctor')
     prescription = []
     test_results = []
     med_info = []
     appointments = []
-    desired_hospital = models.CharField(max_length=50, verbose_name="Desired Hospital")
+    desired_hospital = models.ForeignKey(Hospital, default=None, verbose_name='Desired Hospital')
     e_cont_fname = models.CharField(max_length=50, verbose_name="Emergency Contact: First Name")
     e_cont_lname = models.CharField(max_length=50, verbose_name="Emergency Contact: Last Name")
     e_cont_home_phone = models.BigIntegerField(help_text="No spaces or dashes",

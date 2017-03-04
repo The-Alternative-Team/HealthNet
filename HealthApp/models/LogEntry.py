bdc80cade0015c97d970cc4b0a88f51fa6571e53
Jack Corrigan <jcorrigan14@gmail.com>
2017-03-04 20:25:17 +0000

    added log_action

--- HealthApp/models/LogEntry.py
+++ HealthApp/models/LogEntry.py
@@ -1,10 +1,16 @@
 from django.db import models
+from django.utils import timezone
 
 
 class LogEntry(models.Model):
     userMail = models.CharField(default='', max_length=100, verbose_name='User Email')
     time = models.DateTimeField(default=None, verbose_name='Date Logged')
-    action = models.CharField(default='', max_length=1000, verbose_name='Action')
+    action = models.CharField(default='', max_length=1000, verbose_name='Action Completed')
+
+    @classmethod
+    def log_action(cls, userMail, action):
+        log = cls(userMail=userMail, action=action, time=timezone.now())
+        log.save()
 
     def __str__(self):
-        return self.userMail + " did " + self.action
+        return self.userMail + " did " + self.action + " at " + str(self.time)

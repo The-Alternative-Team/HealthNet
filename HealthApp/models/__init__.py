f720c6bc3108587649dce0e4564acb339b25d84d
Devin Matte <devinmatte@gmail.com>
2017-03-06 03:39:18 +0000

    filling init

---
+++ HealthApp/models/__init__.py
@@ -0,0 +1,7 @@
+from .Appointment import Appointment
+from .Doctor import Doctor
+from .Hospital import Hospital
+from .Nurse import Nurse
+from .Patient import Patient
+from .UserProfile import UserProfile
+from .LogEntry import LogEntry
---
+++ HealthApp/views/__init__.py
@@ -0,0 +1,3 @@
+from .register import register
+from .auth import authForm, unauth
+from .home import home

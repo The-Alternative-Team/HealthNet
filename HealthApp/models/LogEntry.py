b4fff9543c41130e4cc1a65263ef471fd9482d5d
Shadman Nazmuddin <shadman97@gmail.com>
2017-03-06 02:23:50 +0000

    edited __str__ for logs

--- HealthApp/models/LogEntry.py
+++ HealthApp/models/LogEntry.py
@@ -16,4 +16,4 @@ class LogEntry(models.Model):
         log.save()
 
     def __str__(self):
-        return self.userMail + " did " + self.action + " at " + str(self.time)
+        return self.userMail + self.action + " at " + str(self.time)

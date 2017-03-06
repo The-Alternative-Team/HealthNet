fbd9ac642aeeec3cf851c2d3fcc956e676a63efe
Devin Matte <devinmatte@gmail.com>
2017-03-06 03:41:15 +0000

    Fixes Urls

--- HealthApp/urls.py
+++ HealthApp/urls.py
@@ -1,12 +1,10 @@
 from django.conf.urls import url
-from .views import home, auth, register
+from HealthApp.views import home, auth, register
 
 # app_name = 'HealthApp'
 urlpatterns = [
     url(r'^$', home.home, name='home'),
-    url(r'^doctor', home.home, name="doctor"),
-    url(r'^patient', home.home, name="patient"),
     url(r'^login', auth.authForm, name="login"),
     url(r'^logout', auth.unauth, name="logout"),
-    url(r'^register', register.register, name="register"),
+    url(r'^register', register, name="register"),
 ]

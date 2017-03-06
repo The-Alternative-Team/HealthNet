742cefaa26db9da9f811d0bc41761f39973eb25b
Devin Matte <devinmatte@gmail.com>
2017-03-06 03:37:42 +0000

    clearing init

--- HealthApp/forms/__init__.py
+++
@@ -1,4 +0,0 @@
-from .login import Login
-from .register import Register
-from .add_appointment import AddAppointment
-from .update_appointment import SelectAppointment
--- HealthApp/models/__init__.py
+++
@@ -1,7 +0,0 @@
-from .Appointment import Appointment
-from .Doctor import Doctor
-from .Hospital import Hospital
-from .Nurse import Nurse
-from .Patient import Patient
-from .UserProfile import UserProfile
-from .LogEntry import LogEntry

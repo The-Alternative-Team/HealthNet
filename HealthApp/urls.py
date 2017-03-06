82965d42db8ecf19bef0c45ce0c275742958cb6a
Devin Matte <devinmatte@gmail.com>
2017-03-06 03:42:06 +0000

    More urls fixes

--- HealthApp/urls.py
+++ HealthApp/urls.py
@@ -3,7 +3,7 @@ from HealthApp.views import home, auth, register
 
 # app_name = 'HealthApp'
 urlpatterns = [
-    url(r'^$', home.home, name='home'),
+    url(r'^$', home, name='home'),
     url(r'^login', auth.authForm, name="login"),
     url(r'^logout', auth.unauth, name="logout"),
     url(r'^register', register, name="register"),

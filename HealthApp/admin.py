f5008e25a12d1cd24872fd53bfca2f4de052b873
Joel Eager <jee7291@rit.edu>
2017-03-25 19:36:51 +0000

    Improved displaying appointments in the admin interface

--- HealthApp/admin.py
+++ HealthApp/admin.py
@@ -1,14 +1,18 @@
 from django.contrib import admin
 from .models import Appointment, Doctor, Hospital, Nurse, Patient, UserProfile, LogEntry
 
-admin.site.register(Appointment)
 admin.site.register(Doctor)
 admin.site.register(Hospital)
 admin.site.register(Nurse)
 admin.site.register(Patient)
 
 
-# admin.site.register(UserProfile)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = ('start_time', 'doctor', 'patient')
+
+
+admin.site.register(Appointment, AppointmentAdmin)
+
 
 class LogAdmin(admin.ModelAdmin):
     list_display = ('userMail', 'time', 'action')

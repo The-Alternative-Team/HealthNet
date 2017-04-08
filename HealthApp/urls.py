7adcb63e8290f1e67ce1aa0daebfee815f81b3c8
Devin Matte <devinmatte@gmail.com>
2017-04-08 16:18:10 +0000

    Updated Urls

--- HealthApp/urls.py
+++ HealthApp/urls.py
@@ -7,9 +7,9 @@ from HealthApp.views import home, auth, register, uploadFile
 # app_name = 'HealthApp'
 urlpatterns = [
     url(r'^$', home, name='home'),
-    url(r'^login$', auth.authForm, name="login"),
-    url(r'^logout$', auth.unauth, name="logout"),
-    url(r'^register$', register, name="register"),
+    url(r'^login', auth.authForm, name="login"),
+    url(r'^logout', auth.unauth, name="logout"),
+    url(r'^register', register, name="register"),
     url(r'^upload$', uploadFile, name="upload_form"),
 ]
 

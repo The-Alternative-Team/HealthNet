cd366e1aa57f414ced94530ccd02680ac006e2de
Shadman Nazmuddin <shadman97@gmail.com>
2017-04-08 19:36:55 +0000

    Statistics functions completed and ready for testing

--- HealthApp/Statistics.py
+++ HealthApp/Statistics.py
@@ -1,9 +1,12 @@
 from .staticHelpers import *
 from HealthApp.models.AdmissionLog import AdmissionLog
 from HealthApp.models.Patient import Patient
+from HealthApp.models.Prescription import Prescriptions
+from datetime import datetime
 
-
-
+'''
+NOT TESTED
+'''
 def number_admitted_patients():
     return len(get_admitted_patients())
 
@@ -18,4 +21,18 @@ def avg_visits():
 
 
 def avg_length_of_stay():
-    pass
+    total_length_of_stay = datetime.timedelta(0)
+    log_list = AdmissionLog.objects.all().filter(admitStatus=False)
+    num_admissions = len(log_list)
+    for log in log_list:
+        total_length_of_stay += (log.timeDischarged - log.timeAdmitted)
+    avg = total_length_of_stay / num_admissions
+    return avg
+
+def avg_prescriptions():
+    prescription_list = Prescriptions.objects.all()
+    patient_list = Patient.objects.all()
+    prescription_number = len(prescription_list)
+    patient_number = len(patient_list)
+    avg = prescription_number / patient_number
+    return avg

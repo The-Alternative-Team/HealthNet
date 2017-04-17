079a076f6e9767f13fa6415ddd5cd136a6cbd2f6
Joel Eager <jee7291@rit.edu>
2017-04-17 17:00:24 +0000

    Improved statistics generation

--- HealthApp/templatetags/Statistics.py
+++ HealthApp/templatetags/Statistics.py
@@ -1,10 +1,16 @@
 from django import template
 import datetime
+import math
 
 from HealthApp.staticHelpers import *
 from HealthApp.models import AdmissionLog, Patient, Prescription, LogEntry
 
 register = template.Library()
+
+
+# Rounds a float to the given number of digits after the decimal
+def round(num, precision):
+    return math.ceil(num * (10 ** precision)) / (10 ** precision)
 
 
 @register.simple_tag
@@ -18,34 +24,43 @@ def avg_visits():
         admissions_list = AdmissionLog.objects.all()
     except AdmissionLog.DoesNotExist:
         admissions_list = []
+
     try:
         patient_list = Patient.objects.all()
     except Patient.DoesNotExist:
         patient_list = []
+
     admissions_number = len(admissions_list)
     patient_number = len(patient_list)
+
     try:
         avg = admissions_number / patient_number
     except ZeroDivisionError:
         avg = 0
-    return avg
+
+    return round(avg, 2)
 
 
+# in days
 @register.simple_tag
 def avg_length_of_stay():
     total_length_of_stay = datetime.timedelta(0)
+
     try:
         log_list = AdmissionLog.objects.all().filter(admitStatus=False)
         for log in log_list:
             total_length_of_stay += (log.timeDischarged - log.timeAdmitted)
     except AdmissionLog.DoesNotExist:
         log_list = []
+
     num_admissions = len(log_list)
+
     try:
         avg = total_length_of_stay / num_admissions
     except ZeroDivisionError:
         avg = 0
-    return avg
+
+    return round(avg.total_seconds() * (1.1574 * 10 ** -5), 3)
 
 
 @register.simple_tag
@@ -54,17 +69,21 @@ def avg_prescriptions():
         prescription_list = Prescription.objects.all()
     except Patient.DoesNotExist:
         prescription_list = []
+
     try:
         patient_list = Patient.objects.all()
     except Patient.DoesNotExist:
         patient_list = []
+
     prescription_number = len(prescription_list)
     patient_number = len(patient_list)
+
     try:
         avg = prescription_number / patient_number
     except ZeroDivisionError:
         avg = 0
-    return avg
+
+    return round(avg, 2)
 
 @register.simple_tag
 def num_logs():

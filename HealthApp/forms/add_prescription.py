1b12330051c73e65d6d25a57d259266615a5c6b9
Shadman Nazmuddin <shadman97@gmail.com>
2017-04-29 19:39:00 +0000

    commented add prescription form

--- HealthApp/forms/add_prescription.py
+++ HealthApp/forms/add_prescription.py
@@ -1,6 +1,24 @@
+"""
+Add Prescription form
+
+Django form for adding prescriptions.
+
+=== Fields ===
+
+patient -- (CharField) email ID of the patient that is associated with the prescription.
+drug ----- (CharField) name of the drug being prescribed.
+refills -- (IntegerField) number of refills available for the prescription.
+notes ---- (CharField) any notes for the prescriptions.
+
+=== Methods ===
+
+__init__ --------- Initializes the form.
+build_form_dict -- Creates a dictionary of all the add prescription forms for each patient.
+handle_post ------ Creates the prescription given a completed form.
+"""
+
 from django import forms
 from django.utils import timezone
-
 from HealthApp import staticHelpers
 from HealthApp.models import Prescription, LogEntry, Patient
 
@@ -9,7 +27,7 @@ class AddPrescription(forms.ModelForm):
     def __init__(self, patient):
         super().__init__()
         staticHelpers.set_form_id(self, "AddPrescription")
-        
+
         self.fields['patient'] = forms.CharField(widget=forms.HiddenInput(), initial=patient.username)
 
         self.fields['drug'].widget.attrs = {'class': 'form-control', 'placeholder': 'Drug Name'}

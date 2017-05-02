9ad707c56b313a7652e91580419d486fae5c58da
Joel Eager <jee7291@rit.edu>
2017-05-02 21:08:48 +0000

    Functional, but slightly buggy code for validating the UpdatePatient form

--- HealthApp/forms/update_patient.py
+++ HealthApp/forms/update_patient.py
@@ -33,124 +33,93 @@ from HealthApp.models import Doctor, LogEntry, Patient
 
 
 class UpdatePatient(forms.ModelForm):
-    def __init__(self, patient):
-        super().__init__()
+    def __init__(self, patient=None, postData=None):
+        super().__init__(postData, instance=patient)
+
         staticHelpers.set_form_id(self, "UpdatePatient")
 
         # Generate doctor ChoiceField
         doctor_tuple = tuple(Doctor.objects.all().values_list("id", "first_name").order_by("first_name"))
 
-        self.fields['first_name'] = forms.CharField(initial=patient.first_name,
-                                                    widget=forms.TextInput(
-                                                        attrs={'class': 'form-control', 'placeholder': 'First Name'}),
-                                                    label='First Name', max_length=100)
-        self.fields['last_name'] = forms.CharField(initial=patient.last_name,
-                                                   widget=forms.TextInput(
-                                                       attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
-                                                   label='Last Name', max_length=100)
-        self.fields['address_street'] = forms.CharField(initial=patient.address_street,
-                                                        widget=forms.TextInput(
-                                                            attrs={'class': 'form-control', 'placeholder': 'Street'}),
-                                                        label='Street', max_length=100)
-        self.fields['address_city'] = forms.CharField(initial=patient.address_city,
-                                                      widget=forms.TextInput(
-                                                          attrs={'class': 'form-control', 'placeholder': 'City'}),
-                                                      label='City', max_length=100)
-        self.fields['address_state'] = forms.ChoiceField(initial=patient.address_state,
-                                                         widget=forms.Select(
-                                                             attrs={'class': 'form-control', 'placeholder': 'State'}),
-                                                         choices=statesList.STATE_CHOICES, label='State')
-        self.fields['address_zip'] = forms.IntegerField(initial=patient.address_zip,
-                                                        widget=forms.TextInput(
-                                                            attrs={'class': 'form-control', 'placeholder': 'Zip Code'}),
-                                                        label='Zip Code')
-        self.fields['home_phone'] = forms.IntegerField(initial=patient.home_phone,
-                                                       widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                                     'placeholder': '(123)456-7890'}),
-                                                       label='Home Phone Number')
-        self.fields['cell_phone'] = forms.IntegerField(initial=patient.cell_phone,
-                                                       widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                                     'placeholder': '(123)456-7890'}),
-                                                       label='Cell Phone Number')
-        self.fields['e_cont_fname'] = forms.CharField(initial=patient.e_cont_fname,
-                                                      widget=forms.TextInput(
-                                                          attrs={'class': 'form-control', 'placeholder': 'First Name'}),
-                                                      label='Emergency Contact: First Name', max_length=100)
-        self.fields['e_cont_lname'] = forms.CharField(initial=patient.e_cont_lname,
-                                                      widget=forms.TextInput(
-                                                          attrs={'class': 'form-control', 'placeholder': 'Last Name'}),
-                                                      label='Emergency Contact: Last Name', max_length=100)
-        self.fields['e_cont_home_phone'] = forms.IntegerField(initial=patient.e_cont_home_phone,
-                                                              widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                                            'placeholder': '(123)456-7890'}),
-                                                              label='Emergency Contact: Home Phone Number')
-        self.fields['e_cont_cell_phone'] = forms.IntegerField(initial=patient.e_cont_cell_phone,
-                                                              widget=forms.TextInput(attrs={'class': 'form-control',
-                                                                                            'placeholder': '(123)456-7890'}),
-                                                              label='Emergency Contact: Cell Phone Number')
-
-        self.fields['doctor'] = forms.ChoiceField(initial={'doctor': patient.primary_doctor},
-                                                  widget=forms.Select(
-                                                      attrs={'class': 'form-control', 'placeholder': 'Doctor'}),
-                                                  choices=doctor_tuple,
-                                                  label='Doctor')
+        self.fields['first_name'].widget.attrs = {'class': 'form-control', 'placeholder': 'First Name'}
+        self.fields['last_name'].widget.attrs = {'class': 'form-control', 'placeholder': 'Last Name'}
+        self.fields['address_street'].widget.attrs = {'class': 'form-control', 'placeholder': 'Street'}
+        self.fields['address_city'].widget.attrs = {'class': 'form-control', 'placeholder': 'City'}
+        self.fields['address_state'].widget.attrs = {'class': 'form-control', 'placeholder': 'State'}
+        self.fields['address_state'].choices = statesList.STATE_CHOICES
+        self.fields['address_zip'].widget.attrs = {'class': 'form-control', 'placeholder': 'Zip Code'}
+        self.fields['home_phone'].widget.attrs = {'class': 'form-control', 'placeholder': '(123)456-7890'}
+        self.fields['cell_phone'].widget.attrs = {'class': 'form-control', 'placeholder': '(123)456-7890'}
+        self.fields['e_cont_fname'].widget.attrs = {'class': 'form-control', 'placeholder': 'First Name'}
+        self.fields['e_cont_lname'].label = 'Emergency Contact: First Name'
+        self.fields['e_cont_lname'].widget.attrs = {'class': 'form-control', 'placeholder': 'Last Name'}
+        self.fields['e_cont_lname'].label = 'Emergency Contact: Last Name'
+        self.fields['e_cont_home_phone'].widget.attrs = {'class': 'form-control', 'placeholder': '(123)456-7890'}
+        self.fields['e_cont_home_phone'].label = 'Emergency Contact: Home Phone Number'
+        self.fields['e_cont_cell_phone'].widget.attrs = {'class': 'form-control', 'placeholder': '(123)456-7890'}
+        self.fields['e_cont_cell_phone'].label = 'Emergency Contact: Cell Phone Number'
+        self.fields['primary_doctor'].widget.attrs = {'class': 'form-control', 'placeholder': 'Doctor'}
+        self.fields['primary_doctor'].choices = doctor_tuple
+        self.fields['primary_doctor'].label = 'Doctor'
 
     class Meta:
         model = Patient
-        fields = ('first_name', 'last_name')
+        fields = ('first_name', 'last_name', 'address_street', 'address_city', 'address_state', 'address_zip',
+                  'home_phone', 'cell_phone', 'e_cont_fname', 'e_cont_lname', 'e_cont_home_phone',
+                  'e_cont_cell_phone', 'primary_doctor')
 
     @classmethod
-    def handle_post(cls, user_type, patient, post_data):
-
+    def handle_post(cls, user_type, patient, post_data, failedFormDict):
         if user_type == staticHelpers.UserTypes.patient:
-
+            self = UpdatePatient(postData=post_data)
             valid = True
 
             try:
                 patient.home_phone = validate.phone(post_data['home_phone'])
             except forms.ValidationError as e:
-                # TODO ACCESS FIELDS UP THERE ^^^^^^ TO DISPLAY ValidationError CODE
-                cls.fields['home_phone'].add_error('home_phone', e.code)
+                self.fields['home_phone'].add_error('home_phone', e.code)
                 valid = False
 
             try:
                 patient.address_zip = validate.zip(post_data['address_zip'])
             except forms.ValidationError as e:
-                cls.fields['address_zip'].add_error('address_zip', e.code)
+                self.add_error('address_zip', e.code)
                 valid = False
 
             try:
                 patient.cell_phone = validate.phone(post_data['cell_phone'])
             except forms.ValidationError as e:
-                cls.fields['cell_phone'].add_error('cell_phone', e.code)
+                self.fields['cell_phone'].add_error('cell_phone', e.code)
                 valid = False
 
             try:
                 patient.e_cont_home_phone = validate.phone(post_data['e_cont_home_phone'])
             except forms.ValidationError as e:
-                cls.fields['e_cont_home_phone'].add_error('e_cont_home_phone', e.code)
+                self.fields['e_cont_home_phone'].add_error('e_cont_home_phone', e.code)
                 valid = False
 
             try:
                 patient.e_cont_cell_phone = validate.phone(post_data['e_cont_cell_phone'])
             except forms.ValidationError as e:
-                cls.fields['e_cont_home_phone'].add_error('e_cont_home_phone', e.code)
+                self.fields['e_cont_home_phone'].add_error('e_cont_home_phone', e.code)
                 valid = False
 
-            patient.first_name = post_data['first_name']
-            patient.last_name = post_data['last_name']
-            patient.address_street = post_data['address_street']
-            patient.address_city = post_data['address_city']
-            patient.address_state = post_data['address_state']
-            patient.e_cont_fname = post_data['e_cont_fname']
-            patient.e_cont_lname = post_data['e_cont_lname']
-
-            doctor_id = post_data['doctor']
-            patient.primary_doctor = Doctor.objects.get(id=doctor_id)
-
             if not valid:
-                # TODO MANIPULATE HTML/JS TO REFRESH OR WHATEVER
-                print("AHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH!!!!!!!!! INVALID FORM!!!!!!!")
+                # Send the validated form back to the user and mark the form's modal to be auto-opened
+                failedFormDict["profileForm"] = self
+                failedFormDict["autoOpen"] = "updateProfile"
+            else:
+                # Form is good so save it
+                patient.first_name = post_data['first_name']
+                patient.last_name = post_data['last_name']
+                patient.address_street = post_data['address_street']
+                patient.address_city = post_data['address_city']
+                patient.address_state = post_data['address_state']
+                patient.e_cont_fname = post_data['e_cont_fname']
+                patient.e_cont_lname = post_data['e_cont_lname']
 
-            patient.save()
-            LogEntry.log_action(patient.username, "Updated their patient data")
+                doctor_id = post_data['primary_doctor']
+                patient.primary_doctor = Doctor.objects.get(id=doctor_id)
+
+                patient.save()
+                LogEntry.log_action(patient.username, "Updated their patient data")
--- HealthApp/views/home.py
+++ HealthApp/views/home.py
@@ -12,17 +12,16 @@ from HealthApp.models import Patient
 
 
 # Renders the home page with the correct data for the current user
-def render_view(request, user_type, user):
+#   dictOverrides: Fill this with name-form pairs to over ride (such as a validated form)
+def render_view(request, user_type, user, dictOverrides=dict()):
     events = []
     appointments = staticHelpers.find_appointments(user_type, user)
     patients = staticHelpers.find_patients(user_type, user)
     all_patients = Patient.objects.all()
     admitted_patients = staticHelpers.get_admitted_patients()
-    unread_messages = staticHelpers.find_unread_messages(user)
-    sendMessage = SendMessage(user_type)
 
     for app in appointments:
-        # Don't change the title - it'll break the pre-filling of the update appointment form
+        # Don"t change the title - it"ll break the pre-filling of the update appointment form
         title = ""
         if user_type == staticHelpers.UserTypes.patient:
             title = "Appointment with " + str(app.doctor)
@@ -32,24 +31,26 @@ def render_view(request, user_type, user):
             title = "Appointment between " + str(app.patient) + " & " + str(app.doctor)
 
         events.append({
-            'id': str(app.id),
-            'title': title,
-            'description': str(app.notes),
-            'start': str(app.start_time),
-            'end': str(app.end_time)
+            "id": str(app.id),
+            "title": title,
+            "description": str(app.notes),
+            "start": str(app.start_time),
+            "end": str(app.end_time)
         })
 
+    add_app_form = AddAppointment(user_type)
+    update_app_form = UpdateAppointment(user_type)
+    unread_messages = staticHelpers.find_unread_messages(user)
+    sendMessage = SendMessage(user_type)
+    dataDict = {"events": events, "user_type": user_type, "add_app_form": add_app_form, 
+                "update_app_form": update_app_form, "unread_messages": unread_messages, "sendMessage": sendMessage}
+
     if user_type == staticHelpers.UserTypes.patient:
-        form = UpdateAppointment(user_type)
-        add_form = AddAppointment(user_type)
         update_form = UpdatePatient(patient=user)
         prescriptions = staticHelpers.get_patient_prescriptions(user)
         tests = staticHelpers.get_patient_tests(user_type, user, user)
 
-        return render(request, 'HealthApp/index.html',
-                      {"events": events, 'user_type': user_type, 'form': form, 'addForm': add_form,
-                       'profileForm': update_form, 'unread_messages': unread_messages, 'sendMessage': sendMessage,
-                       'prescriptions': prescriptions, 'tests': tests})
+        dataDict.update({"profileForm": update_form, "prescriptions": prescriptions, "tests": tests})
     elif user_type == staticHelpers.UserTypes.doctor:
         set_patient_hospital_forms = SetPatientHospital.build_form_dict(all_patients)
         update_med_info_forms = UpdateMedInfo.build_form_dict(all_patients)
@@ -57,30 +58,22 @@ def render_view(request, user_type, user):
         prescriptions = staticHelpers.get_prescriptions_dict(all_patients)
         tests = staticHelpers.get_tests_dict(user_type, user, all_patients)
         set_patient_admission = staticHelpers.build_set_patient_admission_forms(user_type, all_patients)
-        form = UpdateAppointment(user_type)
-        add_form = AddAppointment(user_type)
 
-        return render(request, 'HealthApp/index.html',
-                      {"events": events, 'user_type': user_type, 'form': form, 'addForm': add_form,
-                       'patients': patients, 'all_patients': all_patients, 'admitted_patients': admitted_patients,
-                       'set_patient_hospital_forms': set_patient_hospital_forms,
-                       'update_med_info_forms': update_med_info_forms,
-                       'set_patient_admission': set_patient_admission, 'add_prescriptions': add_prescriptions,
-                       'prescriptions': prescriptions, 'tests': tests, 'unread_messages': unread_messages,
-                       'sendMessage': sendMessage})
+        dataDict.update({"patients": patients, "all_patients": all_patients, "admitted_patients": admitted_patients,
+                         "set_patient_hospital_forms": set_patient_hospital_forms,
+                         "update_med_info_forms": update_med_info_forms, "set_patient_admission": set_patient_admission,
+                         "add_prescriptions": add_prescriptions, "prescriptions": prescriptions, "tests": tests})
     elif user_type == staticHelpers.UserTypes.nurse:
         set_patient_admission = staticHelpers.build_set_patient_admission_forms(user_type, all_patients)
         update_med_info_forms = UpdateMedInfo.build_form_dict(all_patients)
         prescriptions = staticHelpers.get_prescriptions_dict(all_patients)
-        form = UpdateAppointment(user_type)
-        add_form = AddAppointment(user_type)
 
-        return render(request, 'HealthApp/index.html',
-                      {"events": events, 'user_type': user_type, 'form': form, 'addForm': add_form,
-                       'patients': patients, 'all_patients': all_patients, 'admitted_patients': admitted_patients,
-                       'set_patient_admission': set_patient_admission, 'update_med_info_forms': update_med_info_forms,
-                       'prescriptions': prescriptions, 'unread_messages': unread_messages, 'sendMessage': sendMessage})
+        dataDict.update({"patients": patients, "all_patients": all_patients, "admitted_patients": admitted_patients,
+                         "set_patient_admission": set_patient_admission, "update_med_info_forms": update_med_info_forms,
+                         "prescriptions": prescriptions})
 
+    dataDict.update(dictOverrides)
+    return render(request, "HealthApp/index.html", dataDict)
 
 @login_required(login_url="login/")
 def home(request):
@@ -88,30 +81,28 @@ def home(request):
 
     if user_type == staticHelpers.UserTypes.admin:
         # Redirect an admin over the admin page before trying to pull real user only data
-        return redirect('/admin/')
-    elif request.method == 'POST':
+        return redirect("/admin/")
+    elif request.method == "POST":
         # A form was submitted so handle based on id
-        if request.POST['form_id'] == 'UpdatePatient':
-            UpdatePatient.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'UpdateAppointment' or request.POST['form_id'] == 'AddAppointment':
+        failedFormDict = dict()
+        if request.POST["form_id"] == "UpdatePatient":
+            UpdatePatient.handle_post(user_type, user, request.POST, failedFormDict)
+        elif request.POST["form_id"] == "UpdateAppointment" or request.POST["form_id"] == "AddAppointment":
             UpdateAppointment.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'SetPatientHospital':
+        elif request.POST["form_id"] == "SetPatientHospital":
             SetPatientHospital.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'SendMessage':
+        elif request.POST["form_id"] == "SendMessage":
             SendMessage.handle_post(user, request.POST)
-        elif request.POST['form_id'] == 'AdmitPatient':
+        elif request.POST["form_id"] == "AdmitPatient":
             AdmitPatient.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'DischargePatient':
+        elif request.POST["form_id"] == "DischargePatient":
             DischargePatient.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'AddPrescription':
+        elif request.POST["form_id"] == "AddPrescription":
             AddPrescription.handle_post(user_type, user, request.POST)
-        elif request.POST['form_id'] == 'UpdateMedInfo':
+        elif request.POST["form_id"] == "UpdateMedInfo":
             UpdateMedInfo.handle_post(user_type, user, request.POST)
-        elif 'form_id' not in request.POST:
-            return redirect('/')
 
-        # Form submit has been handled so redirect as a GET (this way refreshing the page works)
-        return redirect('/')
+        return render_view(request, user_type, user, dictOverrides=failedFormDict)
 
     else:
         return render_view(request, user_type, user)
